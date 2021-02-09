cc234d7dc02b9ee85a5b3d54e16b4137df8bcacf
danielle076 <d.vandenakker@novi-education.nl>
2021-02-09 15:27:59 +0100

    updated arrow functie

--- c_arrowfunctie.js
+++ c_arrowfunctie.js
@@ -2,6 +2,17 @@
 // Arrow functies zijn geïntroduceerd in ES6.
 // Met arrow functies kunnen we kortere functie syntax schrijven.
 
+// ARROW SYNTAX (FUNCTION VS ARROW)
+// Normale functie.
+function functionName(arg1, arg2) {
+    // body of function
+}
+
+// Arrow functie.
+const functionName = (arg1, arg2) => {
+    // body of function
+}
+
 // EXAMPLE
 // Normale functie.
 function greet(name) {
@@ -13,7 +24,7 @@ const greeting = greet("Daniëlle");
 console.log(greeting);
 
 // Arrow functie stap 1.
-// Je hebt geen function keyword, ipv daarvan heb je een =>
+// Je hebt geen function keyword, in plaats daarvan heb je een =>.
 const greet2 = (name2) => {
     return "Hoi " + name2;
 }
@@ -30,7 +41,7 @@ const greeting3 = greet3("Daniëlle");
 console.log(greeting3);
 
 // Arrow functie stap 3.
-// Als je maar 1 parameter heb dan kun je de () weglaten.
+// Als je maar 1 parameter hebt dan kun je de () weglaten.
 const greet4 = name4 => "Hoi " + name4;
 
 const greeting4 = greet4("Daniëlle");
