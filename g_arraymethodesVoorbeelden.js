32971495c75df1a23403bf42448f10872b4a6341
danielle076 <d.vandenakker@novi-education.nl>
2021-02-22 17:08:40 +0100

    updated array methodes voorbeelden

--- g_arraymethodesVoorbeelden.js
+++ g_arraymethodesVoorbeelden.js
@@ -1,4 +1,4 @@
-// Gegeven
+// Gegeven Inventors
 const inventors = [
   { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
   { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
@@ -17,20 +17,20 @@ const inventors = [
 
 // 1. Filter de lijst op uitvinders die geboren zijn in de 16e eeuw.
 // Uitkomst: [{ first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 }, { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 }]
-const sixtheenthCentury = inventors.filter((inventor) => {
-  return inventor.year >= 1500 && inventor.year < 1600;
+const inventors16thCentury = inventors.filter(function(inventor) {
+  return inventor.year >= 1500 && inventor.year <= 1600;
 })
 
-console.log(sixtheenthCentury);
+console.log("INVENTORS 16TH CENTURY:", inventors16thCentury);
 
 
 // 2. Maak een array met daarin alle geboortejaren van de uitvinders.
 // Uitkomst: [1879, 1643, 1564, 1867, 1571, 1473, 1858, 1898, 1815, 1855, 1878, 1847];
-const birthYears = inventors.map((inventor) => {
+const birthYears = inventors.map(function(inventor) {
   return inventor.year;
 })
 
-console.log(birthYears);
+console.log("BIRTHYEARS:", birthYears);
 
 
 // 3. Maak een array met daarin alle volledige namen van de uitvinders (voor- en achternaam als één string).
@@ -96,11 +96,28 @@ console.log(inventors);
 
 // 6. Vind de gegevens over de uitvinder wiens achternaam 'Edison' is.
 // Uitkomst: { first: 'Thomas', last: 'Edison', year: 1847, passed: 1931 }
-const outcome = inventors.find((inventor) => {
+const lastNameToFind = "Edison";
+
+function findInventorByLastName(lastName) {
+  for (let i = 0; i < inventors.length; i++) {
+    const inventor = inventors[i];
+    if (inventor.last === lastName) {
+      return inventor;
+    }
+  }
+}
+
+const foundInventor = findInventorByLastName(lastNameToFind);
+console.log("FOUND INVENTOR:", foundInventor);
+
+// KORTE VERSIE
+const foundInventor2 = inventors.find(function(inventor) {
   return inventor.last === "Edison";
 });
 
-console.log(outcome);
+console.log("FOUND INVENTOR2:", foundInventor2);
 
 // KORTERE VERSIE
-// const outcome = inventors.find(inventor => inventor.last === "Edison");
+const foundInventor3 = inventors.find(inventor => inventor.last === "Edison");
+
+console.log("FOUND INVENTOR3:", foundInventor3);
