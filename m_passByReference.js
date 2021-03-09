fb10cf8ba654aac572b2c406d775d97335ab09b1
danielle076 <d.vandenakker@novi-education.nl>
2021-03-09 15:40:10 +0100

    updated pass by reference

--- m_passByReference.js
+++ m_passByReference.js
@@ -1,51 +1,51 @@
 // PASS BY REFERENCE
 // Object datatypes: Object, Array
-// Declaration & assignment
+
+// Verklaring & toewijzing
 let pet = {
-    name: "fluffykins",
+    name: "Freckle",
 };
 
-console.log(pet); // geeft { name: 'fluffykins' }
+console.log(pet); // geeft { name: 'Freckle' }
 
 let petEvilTwin = pet;
 
-console.log(petEvilTwin); // geeft { name: 'fluffykins' }
+console.log(petEvilTwin); // geeft { name: 'Freckle' }
 
-// Re-assignment
-pet.name = "vlekkie"
+// Hernoemen
+pet.name = "Fabian"
 
 // Wat is er gebeurd:
-// 1: in het geheugen van pet = het object { name: 'fluffykins' }
-// 2: in het geheugen van petEvilTwin komt dezelfde waarde als pet = het object { name: 'fluffykins' }
-// 3. het geheugen pet.name wordt vlekkie i.p.v. fluffykins
-// 4. het geheugen van petEvilTwin veranderd mee omdat het hetzelfde geheugen is als pet = het object { name: 'vlekkie' }
-// DUS: we hebben een nieuwe referentie "petEvilTwin" maar verwijst naar hetzelfde object (geen kopie)
-console.log(pet); // geeft { name: 'vlekkie' }
-console.log(petEvilTwin); // geeft { name: 'vlekkie' }
+// 1. in het geheugen van pet = het object { name: 'Freckle' }
+// 2. in het geheugen van petEvilTwin komt dezelfde waarde als pet = het object { name: 'Freckle' }
+// 3. het geheugen pet.name wordt Fabian i.p.v. Freckle
+// 4. het geheugen van petEvilTwin veranderd mee omdat het hetzelfde geheugen is als pet = het object { name: 'Fabian' }
+// DUS: we hebben een nieuwe referentie "petEvilTwin" maar verwijst naar hetzelfde object
+console.log(pet); // geeft { name: 'Fabian' }
+console.log(petEvilTwin); // geeft { name: 'Fabian' }
 
 // Hoe kun je ervoor zorgen dat petEvilTwin niet hetzelfde is als pet?
 // Een kopie maken van het object
 // We maken een nieuw object aan en kopiëren de waarde (name is een primitieve datatype)
 let pet2 = {
-    name: "fluffykins",
+    name: "Freckle",
 };
 
-console.log("PET2", pet2); // geeft PET2 { name: 'fluffykins' }
+console.log("PET2", pet2); // geeft PET2 { name: 'Freckle' }
 
 let petEvilTwin2 = {
     name: pet2.name,
 };
 
-console.log("PET2", petEvilTwin2); // geeft PET2 { name: 'fluffykins' }
+console.log("PET2", petEvilTwin2); // geeft PET2 { name: 'Freckle' }
 
-// Re-assignment
-pet2.name = "vlekkie"
+// Hernoemen
+pet2.name = "Fabian"
 
 // Wat er nu gebeurd:
-// 1: in het geheugen van pet = het object { name: 'fluffykins' }
-// 2: in het geheugen van petEvilTwin komt dezelfde waarde als pet = het object { name: 'fluffykins' }
-// 3. het geheugen pet.name wordt vlekkie i.p.v. fluffykins
-// 4. het geheugen van petEvilTwin veranderd niet mee omdat het een ander geheugen is als pet = het object { name: 'fluffykins' }
-
-console.log("PET2", pet2); // geeft PET2 { name: 'vlekkie' }
-console.log("PET2", petEvilTwin2) // geeft PET2 { name: 'fluffykins' }
+// 1. in het geheugen van pet2 = het object { name: 'Freckle' }
+// 2. in het geheugen van petEvilTwin2 komt dezelfde waarde als pet2 = het object { name: 'Freckle' }
+// 3. het geheugen pet2.name wordt Fabian i.p.v. Freckle
+// 4. het geheugen van petEvilTwin2 veranderd niet mee omdat het een ander geheugen is als pet2 = het object { name: 'Freckle' }
+console.log("PET2", pet2); // geeft PET2 { name: 'Fabian' }
+console.log("PET2", petEvilTwin2) // geeft PET2 { name: 'Freckle' }
