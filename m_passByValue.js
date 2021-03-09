10a9e9af982628591e733cc0c6195b8f82a43cc0
danielle076 <d.vandenakker@novi-education.nl>
2021-03-09 15:52:27 +0100

    updated pass by value

--- m_passByValue.js
+++ m_passByValue.js
@@ -1,18 +1,19 @@
 // PASS BY VALUE
 // Primitive datatype: string, number, boolean, undefined, null
-// Declaration & assignment
+
+// Verklaring & toewijzing
 let firstName = "Danielle";
 console.log(firstName) // geeft Danielle
 
 let alias = firstName;
 
-// Re-assignment
+// Hernoemen
 firstName = "Daantje";
 
 // Wat is er gebeurd:
-// 1: in het geheugen van firstName = Danielle
-// 2: in het geheugen van alias komt dezelfde waarde als firstName = Danielle
+// 1. in het geheugen van firstName = Danielle
+// 2. in het geheugen van alias komt dezelfde waarde als firstName = Danielle
 // 3. het geheugen firstName wordt Daantje i.p.v. Danielle
 // 4. het geheugen van alias veranderd niet
-console.log(firstName); // Daantje
-console.log(alias); // Danielle
+console.log(firstName); // geeft Daantje
+console.log(alias); // geeft Danielle
